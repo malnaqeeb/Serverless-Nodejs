7bec82812c59463545e0ebe97aebe84478316818
malnaqeeb <mosleh.alnaqeeb@gmail.com>
2020-06-11 12:03:18 +0200

    fix delete func

--- src/loan/delete.js
+++ src/loan/delete.js
@@ -1,17 +1,21 @@
-const dynamoDB = require("../dynamodb");
+const dynamoDB = require("../../dynamodb");
 
 module.exports.handler = async (event) => {
   const id = event.pathParameters.id;
   try {
-     await dynamoDB.delete({
-      TableName: process.env.LOAN_TABLE,
-      Key: {
-        id,
-      },
-    }).promise();
+    await dynamoDB
+      .delete({
+        TableName: process.env.LOAN_TABLE,
+        Key: {
+          id,
+        },
+      })
+      .promise();
     return {
       statusCode: 200,
-      body: JSON.stringify({msg: `Loan with id ${id} has been totally deleted`}),
+      body: JSON.stringify({
+        msg: `Loan with id ${id} has been totally deleted`,
+      }),
     };
   } catch (err) {
     return {
